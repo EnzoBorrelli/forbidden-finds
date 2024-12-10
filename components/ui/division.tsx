0121ec6e774f310df9db98ff70aa0d457f63b429
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-12-10 08:27:23 -0300

    Create division.tsx

---
+++ components/ui/division.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Division() {
+  return (
+    <span className="bg-gradient-to-t from-transparent from-1% via-stone-500 to-transparent to-99% h-8 w-1" />
+  );
+}
