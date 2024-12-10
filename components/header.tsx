0affd91599edc47f2693095326a83feb3712669b
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-12-10 08:26:49 -0300

    Create header.tsx

---
+++ components/header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import MetalWallet from "./header/metalWallet";
+import Cart from "./header/cart";
+import User from "./header/user";
+import Division from "./ui/division";
+import SearchBar from "./header/searchBar";
+import HomeBtn from "./header/homeBtn";
+import NavBar from "./header/navBar";
+
+export default function Header() {
+  return (
+    <header className="flex-col w-full px-6 py-4 bg-gradient-to-b divide-y-2 divide-stone-500 from-stone-900 from-80% to-transparent">
+      <div className="flex flex-col md:flex-row items-center justify-between w-full gap-4 md:gap-0 mb-1">
+        <HomeBtn />
+        <SearchBar />
+        <nav>
+          <ul className="flex items-center gap-4">
+            <li>
+              <MetalWallet />
+            </li>
+            <Division />
+            <li>
+              <Cart />
+            </li>
+            <Division />
+            <li>
+              <User />
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <NavBar />
+    </header>
+  );
+}
