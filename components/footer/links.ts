37cd0a3a6a6ec63084b801f243c200c0168b8eef
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-12-11 16:33:56 -0300

    create footer links constants

---
+++ components/footer/links.ts
@@ -0,0 +1,59 @@
+export const infoLinks = [
+  {
+    href: "/about-us",
+    name: "About us",
+  },
+  {
+    href: "/about-this-project",
+    name: "About this project",
+  },
+  {
+    href: "/privacy-policy",
+    name: "Privacy policy",
+  },
+  {
+    href: "/terms-&-conditions",
+    name: "Terms & conditions",
+  },
+  {
+    href: "/currency",
+    name: "Metal Shards / Currency",
+  },
+  {
+    href: "/account",
+    name: "Manage your account",
+  },
+];
+
+export const hyperLinks = [
+  {
+    img: "instagram",
+    href: "https://www.instagram.com/guerrillagames/",
+    label: "@guerrillagames",
+  },
+  {
+    img: "youtube",
+    href: "https://www.youtube.com/guerrillagames",
+    label: "Guerrilla",
+  },
+  {
+    img: "twitter",
+    href: "https://x.com/guerrilla",
+    label: "@Guerilla",
+  },
+  {
+    img: "instagram",
+    href: "https://www.instagram.com/playstation/",
+    label: "@playstation",
+  },
+  {
+    img: "youtube",
+    href: "https://www.youtube.com/playstation/",
+    label: "PlayStation",
+  },
+  {
+    img: "twitter",
+    href: "https://x.com/PlayStation",
+    label: "@PlayStation",
+  },
+];
