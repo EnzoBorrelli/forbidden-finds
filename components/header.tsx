07ef35caacff4a794254643a2e7327083ac073f0
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-12-11 16:54:56 -0300

    add the disclaimer alert

--- components/header.tsx
+++ components/header.tsx
@@ -6,10 +6,12 @@ import Division from "./ui/division";
 import SearchBar from "./header/searchBar";
 import HomeBtn from "./header/homeBtn";
 import NavBar from "./header/navBar";
+import DisclaimerAlert from "./header/disclaimerAlert";
 
 export default function Header() {
   return (
-    <header className="flex-col w-full px-6 py-4 bg-gradient-to-b divide-y-2 divide-stone-500 from-stone-900 from-80% to-transparent">
+    <header className="relative flex-col w-full px-6 py-4 bg-gradient-to-b divide-y-2 divide-stone-500 from-stone-900 from-80% to-transparent">
+      <DisclaimerAlert/>
       <div className="flex flex-col items-center justify-between w-full gap-4 mb-1 md:flex-row md:gap-0">
         <HomeBtn />
         <SearchBar />
