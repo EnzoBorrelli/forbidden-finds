afc39ae9f8b79582be2faa461ceb20cdc18573f8
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-02 18:16:18 -0300

    added user based alert

--- components/header.tsx
+++ components/header.tsx
@@ -7,12 +7,24 @@ import SearchBar from "./header/searchBar";
 import HomeBtn from "./header/homeBtn";
 import NavBar from "./header/navBar";
 import DisclaimerAlert from "./header/disclaimerAlert";
+import { Separator } from "./ui/separator";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { prisma } from "@/lib/prisma";
 
 export default async function Header() {
+  const session = await getServerSession(options);
+  const user = session?.user;
+  const dbUser = user
+    ? await prisma.user.findUnique({
+        where: { email: user?.email },
+      })
+    : null;
+  console.log(dbUser);
 
   return (
-    <header className="relative flex-col w-full px-6 py-4 bg-gradient-to-b divide-y-2 divide-stone-500 from-stone-900 from-80% to-transparent">
-      <DisclaimerAlert />
+    <header className="relative flex-col w-full px-6 py-4 bg-gradient-to-b from-stone-900 from-80% to-transparent">
+      {!user ? <DisclaimerAlert /> : ""}
       <div className="flex flex-col items-center justify-between w-full gap-4 mb-1 md:flex-row md:gap-0">
         <HomeBtn />
         <SearchBar />
@@ -27,11 +39,12 @@ export default async function Header() {
             </li>
             <Division />
             <li>
-              <User isUser={false} />
+              <User user={dbUser} />
             </li>
           </ul>
         </nav>
       </div>
+      <Separator />
       <NavBar />
     </header>
   );
