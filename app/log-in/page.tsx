bb85c4c895fbda20b59f09c082a70f8cd9084e47
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-02 15:42:23 -0300

    login page

---
+++ app/log-in/page.tsx
@@ -0,0 +1,12 @@
+import LogInForm from "@/components/forms/logInForm";
+import SignUpForm from "@/components/forms/signUpForm";
+import React from "react";
+
+export default function LogIn() {
+  return (
+    <div>
+      <SignUpForm />
+      <LogInForm />
+    </div>
+  );
+}
