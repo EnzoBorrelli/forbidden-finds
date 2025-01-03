94b32ad8a34df1b74bd553983cb5355e861253de
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-03 15:57:52 -0300

    dynamic routing

--- components/header/user.tsx
+++ components/header/user.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useState } from "react";
-import { links } from "./user/links";
 import Link from "next/link";
 import { DbUser } from "@/types/dbUser";
 import Image from "next/image";
 import { signIn, signOut } from "next-auth/react";
+
+
+//TODO: add useparams for user
+//TODO: add user options
 
 export default function User({ user }: { user: DbUser | null }) {
   const [menu, setMenu] = useState(true);
@@ -17,7 +20,7 @@ export default function User({ user }: { user: DbUser | null }) {
       >
         <Image
           className="rounded-full text-stone-800 flex justify-center items-center"
-          src={`/avatars/${user?.avatar}.png`}
+          src={`${user ? `/avatars/${user?.avatar}.png` : "/avatars/0.png"}`}
           height={32}
           width={32}
           alt={name.slice(0, 2)}
@@ -29,24 +32,42 @@ export default function User({ user }: { user: DbUser | null }) {
           menu ? "translate-y-0" : "-translate-y-80"
         } transition-transform duration-300 ease-in justify-between p-2 top-10 z-50 right-0 font-semibold tracking-wide divide-y-2 divide-stone-700 bg-stone-800 ring-1 ring-stone-700 text-stone-400`}
       >
-        {links.map((link, index) => (
-          <li key={index}>
+          <li>
             <Link
               className={`hover:text-amber-300 ${
                 user !== null ? "" : "pointer-events-none text-stone-500"
               }`}
-              href={link.href}
+              href={`/${user?.username}`}
             >
-              {link.label}
+              Account
             </Link>
           </li>
-        ))}
+          <li>
+            <Link
+              className={`hover:text-amber-300 ${
+                user !== null ? "" : "pointer-events-none text-stone-500"
+              }`}
+              href="/orders"
+            >
+              Orders
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`hover:text-amber-300 ${
+                user !== null ? "" : "pointer-events-none text-stone-500"
+              }`}
+              href="/discounts"
+            >
+              Discounts
+            </Link>
+          </li>
         <li className="hover:text-amber-300">
-        {user !== null ? (
-        <button onClick={() => signOut()}>Sign Out</button>
-      ) : (
-        <button onClick={() => signIn()}>Log In</button>
-      )}
+          {user !== null ? (
+            <button onClick={() => signOut()}>Sign Out</button>
+          ) : (
+            <button onClick={() => signIn()}>Log In</button>
+          )}
         </li>
       </ul>
     </div>
