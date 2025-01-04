fc26294a33a818816f6bc7243477e39f04e88353
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-04 12:17:01 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
 // pages/_middleware.ts
-import { NextResponse, NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
   const url = req.nextUrl.clone();
 
   // Check if the request is for the /dashboard route
-  if (url.pathname === '/dashboard') {
+  if (url.pathname === "/dashboard") {
     // Check if the user is authenticated and has the admin role
-    if (token && token.role == 'admin') {
+    if (token && token.role == "admin") {
       return NextResponse.next(); // Allow the request to proceed
     }
     // Redirect to /denegado if not an admin
-    url.pathname = '/denegado';
+    url.pathname = "/denegado";
     return NextResponse.redirect(url);
   }
 }
