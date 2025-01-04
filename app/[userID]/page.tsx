1231e4bdd7fb27e115544a595985cf6b9ec6eaa4
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-04 12:17:17 -0300

    added aside nav and tab manager

--- app/[userID]/page.tsx
+++ app/[userID]/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import AsideNav from "@/components/userProfile/asideNav";
+import TabManager from "@/components/userProfile/tabmanager";
 
 export default function Account() {
   return (
-    <main className="w-full h-dvh">account</main>
-  )
+    <main className="flex w-full gap-8 px-10 py-6 h-dvh">
+      <AsideNav />
+      <TabManager/>
+    </main>
+  );
 }
