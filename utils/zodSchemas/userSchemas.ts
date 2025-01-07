e7c33613ab1e0974d93863a8664dca5ba36fa9ef
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-07 14:43:21 -0300

    Update userSchemas.ts

--- utils/zodSchemas/userSchemas.ts
+++ utils/zodSchemas/userSchemas.ts
@@ -39,3 +39,16 @@ export const loginFormSchema = z.object({
     .email("Invalid email"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
 });
+
+export const updatedUserSchema = z.object({
+  id: z.string().min(1, "ID is needed"),
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .max(20, "Username must be at most 20 characters long"),
+  email: z
+    .string()
+    .min(3, "email must be at least 3 characters long")
+    .email("Invalid email"),
+  avatar: z.number().min(1, "Please select an avatar"),
+});
