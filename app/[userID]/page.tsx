0228353d5fc2a3a27479e272b46f70f7c42432bb
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2025-01-07 14:44:17 -0300

    Update page.tsx

--- app/[userID]/page.tsx
+++ app/[userID]/page.tsx
@@ -4,7 +4,7 @@ import TabManager from "@/components/userProfile/tabmanager";
 
 export default function Account() {
   return (
-    <main className="flex w-full gap-8 px-10 py-6 h-dvh">
+    <main className="flex flex-col w-full gap-8 px-10 py-6 md:flex-row h-dvh">
       <AsideNav />
       <TabManager/>
     </main>
